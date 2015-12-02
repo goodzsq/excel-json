56462841acbed30a38792defc03cc637295aeef8
goodzsq <goodzsq@gmail.com>
2015-12-02 14:24:57 +0800

    typescript

--- bin/cmd.js
+++ bin/cmd.js
@@ -5,28 +5,29 @@ var _ = require('lodash');
 var program = require('commander');
 
 program
-  .version('0.0.1')
-  .usage('-f <*.xlsx> -o outputdir [-w]')
-  .option('-w, --watch', 'watch')
-  .option('-f, --file <*.xlsx>', 'to convert xlst file')
-  .option('-o, --output <outpuDir>', 'output directory')
-  .option('-a, --buildaction', 'convert actons file to js')
-  .parse(process.argv);
+	.version('0.0.1')
+	.usage('-f <*.xlsx> -o outputDir [-w]')
+	.option('-w, --watch', 'watch')
+	.option('-t, --typescript', 'convert to typescript')
+	.option('-f, --file <*.xlsx>', 'xlst file')
+	.option('-o, --output <outpuDir>', 'output directory')
+	.option('-a, --buildaction', 'convert actons file to js')
+	.parse(process.argv);
 
-if(!program.file || !fs.existsSync(program.file)){
+if (!program.file || !fs.existsSync(program.file)) {
 	console.error('You MUST specify an excel file with -f');
 	process.exit(1);
 }
-if(!program.output){
+if (!program.output) {
 	console.error('File not exist, You MUST specify an dir with -o');
 	process.exit(1);
 }
 
-function dump(filename, outputDir){
+function dump(filename, outputDir) {
 	var parser = require('../lib/index.js');
 	var data = parser(filename);
 	var buf = "module.exports = {\n";
-	for(var sheetName in data){
+	for (var sheetName in data) {
 		buf += '  ' + sheetName + ": require('./" + sheetName + ".json'),\n";
 		fs.writeFileSync(outputDir + '/' + sheetName + '.json', JSON.stringify(data[sheetName]));
 	}
@@ -35,7 +36,23 @@ function dump(filename, outputDir){
 	console.log('data dumped!');
 }
 
-function dumpActions(filename, outputDir){
+function dumpTypescript(filename, outputDir) {
+	console.log('dumpTypescript')
+	var parser = require('../lib/index.js');
+	var data = parser(filename);
+	var buf = '';
+	var sheets = [];
+	for (var sheetName in data) {
+		sheets.push(sheetName);
+		buf += "import " + sheetName + " from './" + sheetName + ".ts'\n";
+		fs.writeFileSync(outputDir + '/' + sheetName + '.ts', 'export default ' + JSON.stringify(data[sheetName]));
+	}
+	buf += 'export default {' + sheets.toString() + '}';
+	fs.writeFileSync(outputDir + '/index.ts', buf);
+	console.log('data dumped!');
+}
+
+function dumpActions(filename, outputDir) {
 	var parser = require('../lib/buildAction.js');
 	var data = parser(filename);
 	var buf = '';
@@ -49,14 +66,17 @@ function dumpActions(filename, outputDir){
 }
 
 var mydump;
-if(program.buildaction){
-	mydump = _.debounce(dumpActions, 250);
-}else{
-	mydump = _.debounce(dump, 250);
+if (program.buildaction) {
+	mydump = dumpActions
+} else if (program.typescript) {
+	mydump = dumpTypescript
+} else {
+	mydump = dump
 }
+var mydump = _.debounce(mydump, 250);
 
-if(program.watch){
-	fs.watch(program.file, function (event, filename) {
+if (program.watch) {
+	fs.watch(program.file, function(event, filename) {
 		mydump(program.file, program.output);
 	});
 	console.log('watching...');
